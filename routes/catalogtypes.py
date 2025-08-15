03309f7b5df6acd6e6a9139c2a5f02e05b530569
Kevin Martínez Ochoa <kevinochoa@unah.hn>
2025-08-14 22:02:11 -0600

    Add files via upload

--- routes/catalogtypes.py
+++ routes/catalogtypes.py
@@ -7,31 +7,33 @@ from controllers.catalogtypes import (
     update_catalog_type,
     deactivate_catalog_type
 )
-from utils.security import validateuser
+from utils.security import validateadmin
 
 router = APIRouter()
 
 @router.post("/catalogtypes", response_model=CatalogType, tags=["📂 Catalog Types"])
-@validateuser
+@validateadmin
 async def create_catalog_type_endpoint(request: Request, catalog_type: CatalogType) -> CatalogType:
     return await create_catalog_type(catalog_type)
 
-@router.get("/catalogtypes", response_model=list, tags=["📂 Catalog Types"])
-@validateuser
-async def get_catalog_types_endpoint(request: Request, ) -> list:
+@router.get("/catalogtypes", response_model=list[CatalogType], tags=["📂 Catalog Types"])
+@validateadmin
+async def get_catalog_types_endpoint(request: Request, ) -> list[CatalogType]:
     return await get_catalog_types()
 
 @router.get("/catalogtypes/{catalog_type_id}", response_model=CatalogType, tags=["📂 Catalog Types"])
-@validateuser
+@validateadmin
 async def get_catalog_type_by_id_endpoint(request: Request, catalog_type_id: str) -> CatalogType:
     return await get_catalog_type_by_id(catalog_type_id)
 
 @router.put("/catalogtypes/{catalog_type_id}", response_model=CatalogType, tags=["📂 Catalog Types"])
-@validateuser
+@validateadmin
 async def update_catalog_type_endpoint(request: Request, catalog_type_id: str, catalog_type: CatalogType) -> CatalogType:
     return await update_catalog_type(catalog_type_id, catalog_type)
 
-@router.delete("/catalogtypes/{catalog_type_id}", response_model=dict, tags=["📂 Catalog Types"])
-@validateuser
-async def deactivate_catalog_type_endpoint(request: Request, catalog_type_id: str) -> dict:
+@router.delete("/catalogtypes/{catalog_type_id}", response_model=CatalogType, tags=["📂 Catalog Types"])
+@validateadmin
+async def deactivate_catalog_type_endpoint(request: Request, catalog_type_id: str) -> CatalogType:
     return await deactivate_catalog_type(catalog_type_id)
+
+
